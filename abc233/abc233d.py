f0224574f65ffe7e4976e2ea4d50f26b8e1da6dc
tru22 <vlimptonalpha@gmail.com>
2022-03-05 01:56:33 +0900

    abc233d ended

--- abc233/abc233d.py
+++ abc233/abc233d.py
@@ -1,16 +1,20 @@
+# どうしてACなのかわからん…
 from collections import defaultdict
 
-N, K = map(int, input().split())
+N, K = map(int, input().split()) #6, 5
 numbers = list(map(int, input().split()))
 cumsum = [0]
 
 for i in range(N):
     cumsum.append(cumsum[i] + numbers[i])
 
-print(numbers)
+#print(numbers)
 print(cumsum)
 mp = defaultdict(int)
-for i in range(0, N + 1):
-    mp[cumsum[i]] += 1
-
-print(mp)
+ans = 0
+for i in range(1, N + 1):
+    mp[cumsum[i - 1]] += 1
+    ans += mp[cumsum[i] - K]
+    print(mp)
+    print(mp[cumsum[i] - K])
+    #print(ans)
